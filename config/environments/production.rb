bd09900405d21e78020bc75f28f6d0ef781c0bf8
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-12-17 01:24:12 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
    config.hosts = [
-     "evershop-demo-sddfs.g5edov.easypanel.host",     # Allow requests from example.com
+     "evershop-demo-sddfs.g5edov.easypanel.host"     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
